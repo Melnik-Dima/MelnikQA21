dc51004bec09f6591bda2e2b2dc933259768b006
Dmitry Melnik <familymelniks@gmail.com>
2019-08-22 15:03:19 +0300

    Wiki test updated for firefox

--- wikipedia-web-tests/src/test/java/com/telran/tests/SearchWikiTest.java
+++ wikipedia-web-tests/src/test/java/com/telran/tests/SearchWikiTest.java
@@ -3,6 +3,7 @@ package com.telran.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SearchWikiTest {
@@ -10,8 +11,10 @@ public class SearchWikiTest {
     WebDriver driver;
     @Test
     public void setUp() throws InterruptedException {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.get("https://en.wikipedia.org");
+        driver.findElement(By.name("search")).click();
+        driver.findElement(By.name("search")).clear();
         driver.findElement(By.name("search")).sendKeys("java");
         Thread.sleep(6000);
         driver.quit();
